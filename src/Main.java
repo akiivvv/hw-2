87e7f72020abfb7e770a749ed5a659f8b7991727
Vika <viktoria14.ko@gmail.com>
2024-09-29 12:19:50 +0300

    pr 2

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.print("Тип: int\n");
+        System.out.print("Розмір: " + Integer.BYTES + " байт\n");
+        System.out.print("Мінімальне значення: " + Integer.MIN_VALUE + "\n");
+        System.out.print("Максимальне значення: " + Integer.MAX_VALUE + "\n\n");
+
+        System.out.print("Тип: double\n");
+        System.out.print("Розмір: " + Double.BYTES + " байт\n");
+        System.out.print("Мінімальне значення: " + Double.MIN_VALUE + "\n");
+        System.out.print("Максимальне значення: " + Double.MAX_VALUE + "\n\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть ціле число (int): ");
+        if (scanner.hasNextInt()) {
+            int intValue = scanner.nextInt();
+            System.out.print("Введене значення (int): " + intValue + "\n\n");
+        } else {
+            System.out.print("Некоректне введення для типу int.\n\n");
+            scanner.next();
+        }
+
+        System.out.print("Введіть число з плаваючою комою (double): ");
+        if (scanner.hasNextDouble()) {
+            double doubleValue = scanner.nextDouble();
+            System.out.print("Введене значення (double): " + doubleValue + "\n");
+        } else {
+            System.out.print("Некоректне введення для типу double.\n");
+        }
+    }
+}
+
